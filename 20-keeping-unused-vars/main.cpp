9725986f8c0b4a0982104ee65129d4da1e8914fa
Raushan Kumar <raushan@Raushans-MacBook-Air.local>
2026-07-06 22:22:21 +0530

    learned some new topics

---
+++ 20-keeping-unused-vars/main.cpp
@@ -0,0 +1,17 @@
+// This allows us to tell the compiler that we’re okay with a variable being unused. The compiler will not generate unused variable warnings for such variables.
+
+#include <iostream>
+
+int main()
+{
+    [[maybe_unused]] double pi { 3.14159 };  // Don't complain if pi is unused
+    [[maybe_unused]] double gravity { 9.8 }; // Don't complain if gravity is unused
+    [[maybe_unused]] double phi { 1.61803 }; // Don't complain if phi is unused
+
+    std::cout << pi << '\n';
+    std::cout << phi << '\n';
+
+    // The compiler will no longer warn about gravity not being used
+
+    return 0;
+}
